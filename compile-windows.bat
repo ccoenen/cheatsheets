d728464df20027c5cc8c7f4a2f5d148e32b59400
Claudius Coenen <opensource@amenthes.de>
2022-01-04 14:56:56 +0100

    build program now only with neccessary lines

--- compile-windows.bat
+++ compile-windows.bat
@@ -1,9 +1,3 @@
 setlocal
 PATH="C:\Program Files\Inkscape\bin\";%PATH%
-
-::latex --shell-escape css-basics-de.tex
-::biber css-basics-de
-::latex --shell-escape css-basics-de.tex
-::pdflatex --shell-escape css-basics-de.tex
-
 latexmk -output-directory=build -pdflatex="pdflatex --shell-escape %%O %%S" -pdf %1
